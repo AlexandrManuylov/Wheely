6378bd5a5ccbb36d97ba849012877e2b0914fc2d
Alexandr Manuylov <alexandr.manuylov@chaynik.org>
2016-04-29 10:40:05 +0300

    login design

--- app/src/main/java/org/chaynik/wheely/LoginFragment.java
+++ app/src/main/java/org/chaynik/wheely/LoginFragment.java
@@ -7,7 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener {
     public static final String TAG = "LoginFragment";
 
     @Nullable
@@ -15,6 +15,25 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.login_fragment, container, false);
+        view.findViewById(R.id.text_sign_in).setOnClickListener(this);
         return view;
+    }
+    @Override
+    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.button_start_service:
+//                if (!WheelyUtils.isServiceRunning(this, WebSocketService.class)) {
+//                    Log.i("Test", "Activity: onCreate");
+//                    startService(new Intent(this, WebSocketService.class));
+//                } else {
+//                    Log.i("Test", "Activity: onCreated");
+//                }
+//                break;
+//            case R.id.button_stop_service:
+//                Log.i("Test", "Activity: onStop");
+//                stopService(new Intent(this, WebSocketService.class));
+//                break;
+//        }
+
     }
 }
--- app/src/main/java/org/chaynik/wheely/MainActivity.java
+++ app/src/main/java/org/chaynik/wheely/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
         showFragmentByTag(LoginFragment.TAG);
 //        findViewById(R.id.button_start_service).setOnClickListener(this);
 //        findViewById(R.id.button_stop_service).setOnClickListener(this);
